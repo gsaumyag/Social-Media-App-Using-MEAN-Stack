1cf53c92a1d0111a5661943ecdab04d084ee894a
Saumyajit Saha <gsaumyag@gmail.com>
2022-07-26 04:49:40 +0530

    "Base files, Committed"

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent{
+  posts = [
+    {
+      title: 'First Post',content: 'This is the firts post\'s content'
+    },
+    {
+      title: 'Second Post',content: 'This is the second post\'s content'
+    },
+    {
+      title: 'Third Post',content: 'This is the third post\'s content'
+    }
+  ];
+}
