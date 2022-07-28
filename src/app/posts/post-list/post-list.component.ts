de685b47b20e0a9ca93bef98d5203c7101b14a38
Saumyajit Saha <gsaumyag@gmail.com>
2022-07-29 04:00:32 +0530

    Creating a Post Model

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input} from "@angular/core";
 
+import { Post } from "../post.model";
+
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
@@ -18,6 +20,6 @@ export class PostListComponent{
   //     title: 'Third Post',content: 'This is the third post\'s content'
   //   }
   // ];
-  @Input() posts = [];
+  @Input() posts: Post[] = [];
   //posts = [];
 }
