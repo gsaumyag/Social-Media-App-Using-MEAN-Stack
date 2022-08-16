9778a3ffe1414f183b7a5eafdf5d50eaaad156f7
Saumyajit Saha <gsaumyag@gmail.com>
2022-08-16 12:57:04 +0530

    Understanding CORS

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,19 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, DELETE, OPTIONS"
+      );
+    next();
+});
+
 app.use('/api/posts',(req, res, next) =>
 {
   const posts = [
@@ -11,8 +24,8 @@ app.use('/api/posts',(req, res, next) =>
       content: 'This is coming from the server'
     },
     {
-      id:'#001',
-      title: 'First server-side post',
+      id:'#002',
+      title: 'Second server-side post',
       content: 'This is coming from the server'
     }
   ];
