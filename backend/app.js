b31168481bbdfc28fe9837b9af64d52ef49de0f2
Saumyajit Saha <gsaumyag@gmail.com>
2022-08-26 16:36:27 +0530

    Storing Data in a Database

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const Post = require('./models/post');
 
 const app = express();
 
-mongoose.connect("mongodb+srv://gsaumyag:Plusfire123.@cluster0.yiljumo.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://gsaumyag:Plusfire123.@cluster0.yiljumo.mongodb.net/node-angular?retryWrites=true&w=majority")
 .then(() =>{
   console.log("Connected to Database!");
 })
@@ -35,7 +35,7 @@ app.post('/api/posts',(req, res, next) => {
     title : req.body.title,
     content : req.body.content
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
